b6df80543a7f24a08838751e63b2fdfc20da197e
Gnlow <gnlow@protonmail.com>
2025-01-03 04:46:02 +0900

    some style

--- src/board.ts
+++ src/board.ts
@@ -14,10 +14,14 @@ export const Board = html`
 <style>
     Board {
         width: 100%;
-        border: 1px solid black;
+        border: 5px solid var(--black);
+        padding: 5px;
 
         display: grid;
-        grid: repeat(5, 1fr) / repeat(5, 1fr);
+        grid: repeat(6, 1fr) / repeat(6, 1fr);
+        grid-gap: 5px;
+
+        border-radius: 15px;
     }
 </style>
 <Board>
@@ -27,6 +31,8 @@ export const Board = html`
             width: 100%;
             height: 100%;
             aspect-ratio: 1;
+
+            border-radius: 5px;
         }
         Tile[state="0"] {
             background: var(--black);
@@ -35,8 +41,14 @@ export const Board = html`
             background: var(--white);
         }
     </style>
-    ${boardState.flatMap(row => row.map(state => html`<Tile
+    ${boardState.flatMap((row, rowI) => row.map((state, colI) => html`<Tile
         state="${state}"
+        col="${colI}"
+        row="${rowI}"
+        style="
+            grid-column: ${colI + 2};
+            grid-row: ${rowI + 2};
+        "
     />`))}
 </Board>
 `
