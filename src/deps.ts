7b79c00572b5d3b5074925b6a0e37215479a21e3
Gnlow <gnlow@protonmail.com>
2025-01-08 01:23:30 +0900

    no lock

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
 export { html, render } from "https://esm.sh/lit-html@3.2.1"
-export { map } from "https://esm.sh/lit@3.2.1/directives/map"
+export { map } from "https://esm.sh/lit-html@3.2.1/directives/map"
